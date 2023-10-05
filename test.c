8bd2098ca4f441acdeb241f8c016d851a4c7aa09
HieuNguyen144 <ngaynangmois@gmail.com>
2023-10-06 06:56:24 +0700

    all

---
+++ test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+    bool Hieu_dep_trai = true;
+    bool Hieu_xau_trai = false;
+
+    int x =10, y =9;
+    printf("%d\n", Hieu_dep_trai);
+    printf("%d\n", Hieu_xau_trai);
+    printf("%d\n", x > y);
+
+    return 0;
+}
